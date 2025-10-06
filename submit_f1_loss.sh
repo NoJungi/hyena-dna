4807a507f16992b5b99f94ee1118bc7b59f6850f
NoJungi <nora.jungierek@stud.uni-greifswald.de>
2025-10-06 17:35:18 +0000

    submit files to run on HPC

---
+++ submit_f1_loss.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#SBATCH --job-name=train_4_256
+#SBATCH --output=slurm/train_full_CNN_BEND_1026_max_length_00006_lr_256_d_model_4_order_32_batch_size_50_epochs_%a_layers_%A.out
+#SBATCH --error=slurm/train_full_CNN_BEND_1026_max_length_00006_lr_256_d_model_4_order_32_batch_size_50_epochs_%a_layers_%A.err
+#SBATCH --nodes=1
+#SBATCH --partition=vision
+#SBATCH --gpus=1
+#SBATCH --time=10:00:00
+#SBATCH --mem=100gb
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --array=1-5%2
+
+
+D_MODEL=256
+N_LAYER=${SLURM_ARRAY_TASK_ID}
+ORDER=4     # min: 2
+
+
+MAX_LENGTH=1026
+DECODER="CNN_BEND"
+LR=0.00006
+BATCH_SIZE=32
+EPOCHS=50
+
+
+# make name for output dir
+LR_name=$(echo $LR | awk -F. '{print $2}') # only digits after '.'
+
+DATE=$(date '+%Y-%m-%d_%H-%M-%S')
+# Dynamischen Run-Namen und Ordner bauen
+RUN_NAME="${ORDER}_order_${BATCH_SIZE}_batch_size_${EPOCHS}_epochs_${DATE}"
+OUT_DIR="./outputs/train_full/CCE_F1_loss/${DECODER}_${MAX_LENGTH}_max_length_${LR_name}_lr_${D_MODEL}_d_model/${N_LAYER}_layers/${RUN_NAME}"
+WANDB_RUN_NAME="$train_full_F1_loss_${DECODER}_${N_LAYER}_layers_${ORDER}_order_${MAX_LENGTH}_max_len_${D_MODEL}_d_model_${BATCH_SIZE}_batch_size_${EPOCHS}_epochs_${LR_name}_lr"
+
+
+echo "start training with:"
+echo "  d_model            = ${D_MODEL}"
+echo "  n_layer            = ${N_LAYER}"
+echo "  order              = ${ORDER}"
+echo "  max_len            = ${MAX_LENGTH}"
+echo "  decoder            = ${DECODER}"
+echo "  lr                 = ${LR}"
+echo "  batch              = ${BATCH_SIZE}"
+echo "  epochs             = ${EPOCHS}"
+echo "  Run-Name           = ${RUN_NAME}"
+echo "  Output             = ${OUT_DIR}"
+
+# Training starten mit Hydra-Overrides
+time python -m train experiment=hg38/gene_finding \
+dataset.max_length=$MAX_LENGTH \
+dataset.batch_size=$BATCH_SIZE \
+model.d_model=$D_MODEL \
+model.n_layer=$N_LAYER \
+model.layer.order=$ORDER \
+decoder._name_=$DECODER \
+trainer.max_epochs=$EPOCHS \
+optimizer.lr=$LR \
+wandb.name=$WANDB_RUN_NAME \
+hydra.run.dir=$OUT_DIR
+
+sstat -j "$SLURM_JOB_ID".batch --format=JobID,MaxVMSize
---
+++ submit_fine_tuning_cce.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --job-name=tiny_128_freeze
+#SBATCH --output=slurm/fine_tuning_CNN_BEND_tiny_128_d_model_freeze_00006_lr_32_batch_size_50_epochs_%j.out
+#SBATCH --error=slurm/fine_tuning_CNN_BEND_tiny_128_d_model_freeze_00006_lr_32_batch_size_50_epochs_%j.err
+#SBATCH --nodes=1
+#SBATCH --partition=vision
+#SBATCH --gpus=1
+#SBATCH --time=10:00:00
+#SBATCH --mem=100gb
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+
+PRETRAINED_MODEL="hyenadna-tiny-1k-seqlen" # "hyenadna-tiny-1k-seqlen-d256" #
+D_MODEL=128 #128
+FREEZE=True  # 
+
+DECODER="CNN_BEND"
+LR=0.00006
+BATCH_SIZE=32
+EPOCHS=50
+
+
+# make name for output dir
+LR_name=$(echo $LR | awk -F. '{print $2}') # only digits after '.'
+# 'freeze' if FREEZE is True else 'fully_tuned'
+FREEZE_name=$(if [ "$FREEZE" = True ]; then echo "freeze"; else echo "fully_tuned"; fi)
+PRETRAINED_MODEL_PATH="/home/s-nojung/Masterarbeit/Code/hyena-dna/pretrained_weights/${PRETRAINED_MODEL}/weights.ckpt"
+
+DATE=$(date '+%Y-%m-%d_%H-%M-%S')
+# Dynamischen Run-Namen und Ordner bauen
+RUN_NAME="${FREEZE_name}_${BATCH_SIZE}_batch_size_${EPOCHS}_epochs_${DATE}"
+OUT_DIR="./outputs/fine_tuning/CCE_loss/${DECODER}_${PRETRAINED_MODEL}_${LR_name}_lr/${RUN_NAME}"
+WANDB_RUN_NAME="$fine_tuning_CCE_loss_${DECODER}_${PRETRAINED_MODEL}_${FREEZE_name}_${BATCH_SIZE}_batch_size_${EPOCHS}_epochs_${LR_name}_lr"
+
+
+echo "start fine tuning with:"
+echo "  pretrained_model   = ${PRETRAINED_MODEL}"
+echo "  d_model            = ${D_MODEL}"
+echo "  freeze             = ${FREEZE}"
+echo "  decoder            = ${DECODER}"
+echo "  lr                 = ${LR}"
+echo "  batch              = ${BATCH_SIZE}"
+echo "  epochs             = ${EPOCHS}"
+echo "  Output             = ${OUT_DIR}"
+echo "  wandb Run-Name     = ${WANDB_RUN_NAME}"
+
+# Training starten mit Hydra-Overrides
+time python -m train experiment=hg38/gene_finding_cce \
+dataset.batch_size=$BATCH_SIZE \
+model.d_model=$D_MODEL \
+decoder._name_=$DECODER \
+trainer.max_epochs=$EPOCHS \
+optimizer.lr=$LR \
+wandb.name=$WANDB_RUN_NAME \
+hydra.run.dir=$OUT_DIR \
+train.pretrained_model_path=$PRETRAINED_MODEL_PATH \
+train.pretrained_model_state_hook.freeze_backbone=$FREEZE
+
+sstat -j "$SLURM_JOB_ID".batch --format=JobID,MaxVMSize
